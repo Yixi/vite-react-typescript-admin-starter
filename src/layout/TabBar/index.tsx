0ff3938512a640aec227c20bd0f21fc7e4621253
Yixi <lyh1112@gmail.com>
2023-09-20 11:01:09 +0800

    add tab close

--- src/layout/TabBar/index.tsx
+++ src/layout/TabBar/index.tsx
@@ -1,33 +1,73 @@
 import { routeConfig } from '@root/routes'
 import { parseRoutesFlatten } from '@root/utils/parseRoute'
-import { Tabs, theme } from 'antd'
-import { TabsProps } from 'antd/lib'
-import { find } from 'lodash'
+import { Space, Tabs, theme } from 'antd'
+import { find, findIndex, last, map } from 'lodash'
 import React, { useEffect, useMemo } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import styles from './index.module.less'
+import { CloseOutlined } from '@ant-design/icons'
+import { RouteInfo } from '@root/types/base'
 
 const TabBar = () => {
   const navigate = useNavigate()
   const { pathname } = useLocation()
   const { token } = theme.useToken()
-  const [tabs, setTabs] = React.useState<TabsProps['items']>([])
+  const [tabs, setTabs] = React.useState<
+    { pathname: string; route: RouteInfo }[]
+  >([])
   const [activeKey, setActiveKey] = React.useState(pathname)
 
   const flattenRoutes = useMemo(() => {
     return parseRoutesFlatten(routeConfig)
   }, [routeConfig])
 
+  const onRemoveTab = (pathname: string) => {
+    // console.log(pathname)
+    // console.log(tabs)
+    const tabIndex = findIndex(tabs, { pathname: pathname })
+    // console.log(tabIndex)
+    const newTabs = tabs.filter((tab) => tab.pathname !== pathname)
+    setTabs(newTabs)
+
+    if (pathname === location.pathname) {
+      // console.log(newTabs)
+      let nextTab = newTabs[tabIndex]
+      if (!nextTab) {
+        nextTab = last(newTabs)
+      }
+      if (!nextTab) {
+        nextTab = { pathname: '/', route: null }
+      }
+      navigate(nextTab.pathname)
+    }
+  }
+
   useEffect(() => {
-    if (!find(tabs, { key: pathname })) {
+    console.log(pathname)
+    if (!find(tabs, { pathname })) {
       const route = find(flattenRoutes, { path: pathname })
-      setTabs((prev) => [
-        ...prev,
-        { key: pathname, label: route?.meta?.title || '' },
-      ])
+      setTabs((prev) => [...prev, { pathname, route }])
     }
     setActiveKey(pathname)
   }, [pathname, flattenRoutes])
+
+  const tabItems = useMemo(() => {
+    return map(tabs, (tab) => ({
+      key: tab.pathname,
+      label: (
+        <Space>
+          <span>{tab.route?.meta?.title || ''}</span>
+          <CloseOutlined
+            className={styles.close}
+            onClick={(event) => {
+              event.stopPropagation()
+              onRemoveTab(tab.pathname)
+            }}
+          />
+        </Space>
+      ),
+    }))
+  }, [tabs])
 
   const onChange = (key: string) => {
     navigate(key)
@@ -41,7 +81,7 @@ const TabBar = () => {
       }}
       className={styles['tab-bar']}
     >
-      <Tabs items={tabs} activeKey={activeKey} onChange={onChange} />
+      <Tabs items={tabItems} activeKey={activeKey} onChange={onChange} />
     </div>
   )
 }
